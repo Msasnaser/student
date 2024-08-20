8fc4be929926938d7ad88f94ed2fbd3023722439
Masanaser <masadawood29@gmail.com>
2024-08-20 23:59:30 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace studentExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
